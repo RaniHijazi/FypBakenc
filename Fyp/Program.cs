ee45252368de0ba31f32bb585ad674525c19e57b
wadihissa1 <148622804+wadihissa1@users.noreply.github.com>
2024-05-18 20:24:41 +0300

    updating lunch setting

--- Fyp/Program.cs
+++ Fyp/Program.cs
@@ -15,7 +15,6 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 var services = builder.Services;
-
 services.AddControllers();
 services.AddEndpointsApiExplorer();
 
